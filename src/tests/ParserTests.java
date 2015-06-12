9f9a53fd54a77a77f8261d6cc7d70f5d3648f564
Luiz Felix <lzcfelix@gmail.com>
2015-06-12 10:49:28 +1000

    Initial commit.

---
+++ src/tests/ParserTests.java
@@ -0,0 +1,19 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ParserTests {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
